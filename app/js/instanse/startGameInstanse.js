69767053a73941620a62c8389e9e43cb6c6ae62e
TsvirenkoSweet <andrei.tsvirenko@gmail.com>
2018-09-20 18:06:09 +0300

    Доработана логика перетаскивания + прикручен таймер

--- app/js/instanse/startGameInstanse.js
+++ app/js/instanse/startGameInstanse.js
@@ -1,4 +1,5 @@
-startGameInstance = function (settings){
+var _timer = null;
+startGameInstance = function (_settings){
     this._settings = {
         selector:''
     };
@@ -32,6 +33,10 @@ startGameInstanceProto._onToggleClick = function(event){
 
 //Плавное затухание кнопки "Начать игру"
 startGameInstanceProto._startButtonClick = function(el){
+    
+    _timer = new Timer(1000, function(){
+        document.getElementById('timer').innerHTML = this.count;
+      }).run();
     var opacity = 1;
     var time = 800;
     var timer = setInterval( function() {
@@ -75,7 +80,7 @@ startGameInstanceProto._createGameArea = function(){
             div.appendChild(uploadImage);
         }
         if(i == 1){
-            div.className = "customizeBlock pazleBlock";
+            div.className = "customizeBlock  pazleBlock";
         }
         if(i == 2){
             div.className = "findPuzleContainer pazleBlock";
@@ -141,11 +146,11 @@ startGameInstanceProto._createPazzleArea = function(el){
                     var x = 0;
                     for (var j = 0; j < 4; j++) {
                         var pazlMoveBlock = document.createElement("div");
-                        pazlMoveBlock.dataset.movePazleId = j + 1 + step;
+                        var allStep = j + 1 + step;
+                        pazlMoveBlock.dataset.movePazleId = allStep;
                         pazlMoveBlock.style.backgroundImage = "url('"+ putBGImage +"')";
                         pazlMoveBlock.classList.add('move-puzle');
-                        pazlMoveBlock.setAttribute('id', 'move-puzle-'+ j + step);
-                        pazlMoveBlock.innerHTML = j + 1 + step;
+                        pazlMoveBlock.setAttribute('id', 'move-puzle-'+ allStep);
                         pazlMoveBlock.style.backgroundPositionY = y +'px';
                         if (j % 4 == 0){
                             x = 0;
@@ -163,30 +168,17 @@ startGameInstanceProto._createPazzleArea = function(el){
                 }
                 var targetArr = this._rundomSort(moveArray);
                 for (var f = 0; f < targetArr.length; f++) {
-                    console.log(targetArr[f]);
                     targetArr[f].setAttribute('draggable', true);
-                    targetArr[f].setAttribute('ondragstart', 'drag(event)');
                     findPuzleContainer.appendChild(targetArr[f]);
                 }
-document.getElementById('pazleBlock-1').setAttribute('ondrop', 'drop(event)');
-document.getElementById('pazleBlock-1').setAttribute('ondragover', 'allowDrop(event)');
-document.getElementById('pazleBlock-2').setAttribute('ondrop', 'drop(event)');
-document.getElementById('pazleBlock-2').setAttribute('ondragover', 'allowDrop(event)');
-                // var doubles = this._rundomSort(moveArray).map(function(i, t) {
-                //     console.log(i, t);
-                //     });
-                // console.log(doubles);
-                
-
-                // for(var z = 0; z < 16; z++){
-                //     var res = '';
-                //     console.log(moveArray);
-                //     console.log(res = moveArray[Math.floor(Math.random()*moveArray.length)]);
-                //     console.log(res.dataset.movePazleId);
-                //     console.log(delete moveArray[res.dataset.movePazleId - 1]);
-                //     rundomArr.push(res.dataset.movePazleId);
-                // }
-                // console.log('итог'+ rundomArr);
+                var draggable = new DragNSort({
+                  container: document.querySelector('#pazleBlock-2'),
+                  targetcontainer: document.querySelector('#pazleBlock-1'),
+                  itemClass: 'move-puzle',
+                  dragStartClass: 'drag-start',
+                  dragEnterClass: 'drag-enter'
+                });
+                draggable.init();
             }
             else{
                 var pazlBlock = document.createElement("div");
@@ -203,6 +195,184 @@ document.getElementById('pazleBlock-2').setAttribute('ondragover', 'allowDrop(ev
         
     }
 }
+
+DragNSort = function (config) {
+    this.$activeItem = null;
+    this.$putAreaItem = null;
+    this.$container = config.container;
+    this.$targetcontainer = config.targetcontainer;
+    this.$targetitems = this.$targetcontainer.childNodes;
+    this.$items = this.$container.querySelectorAll('.' + config.itemClass);
+    this.dragStartClass = config.dragStartClass;
+    this.dragEnterClass = config.dragEnterClass;
+  }
+  DragNSortProto = DragNSort.prototype;
+  DragNSortProto.removeClasses = function () {
+      [].forEach.call(this.$items, function ($item) {
+          $item.classList.remove(this.dragStartClass, this.dragEnterClass);
+    }.bind(this));
+  };
+  
+  DragNSortProto.on = function (elements, eventType, handler) {
+      [].forEach.call(elements, function (element) {
+      element.addEventListener(eventType, handler.bind(element, this), false);
+    }.bind(this));
+  };
+  
+  DragNSortProto.onDragStart = function (_this, event) {
+    _this.$activeItem = this;
+    var setDataObj = new Object();
+    setDataObj.dataMovePazzleId = this.dataset.movePazleId;
+    setDataObj.id = this.id;
+    setDataObj.backgroundPosition = this.style.backgroundPosition;
+
+    this.classList.add(_this.dragStartClass);
+    event.dataTransfer.effectAllowed = 'move';
+    event.dataTransfer.setData('text/plain',JSON.stringify(setDataObj));
+  };
+  
+  DragNSortProto.onDragEnd = function (_this, event) {
+        _this.$activeItem = this;
+      this.classList;
+      this.classList.remove(_this.dragStartClass);
+  };
+  
+  DragNSortProto.onDragEnter = function (_this) {
+      this.classList.add(_this.dragEnterClass);
+  };
+  
+  DragNSortProto.onDragLeave = function (_this) {
+      this.classList.remove(_this.dragEnterClass);
+  };
+  
+  DragNSortProto.onDragOver = function (_this, event) {
+    if (event.preventDefault) {
+    event.preventDefault();
+    }
+  
+    event.dataTransfer.dropEffect = 'move';
+    return false;
+  };
+  
+  DragNSortProto.onDrop = function (_this, event) {
+      if (event.stopPropagation) {
+      event.stopPropagation();
+    }
+    _this.$putAreaItem = this;
+    console.log(_this.$putAreaItem);
+    console.log(_this.$putAreaItem.dataset.findPazleId);
+    console.log(_this.$activeItem.dataset.movePazleId);
+    
+    console.log(_this);
+    console.log(event);
+    if(_this.$putAreaItem.parentNode.id == _this.$targetcontainer.id){
+        if(_this.$putAreaItem.dataset.findPazleId !== _this.$activeItem.dataset.movePazleId)    return false;
+        _this.$putAreaItem.id = _this.$activeItem.id;
+        _this.$putAreaItem.dataset.movePazleId = _this.$activeItem.dataset.movePazleId;
+        _this.$putAreaItem.style.backgroundPosition = _this.$activeItem.style.backgroundPosition;
+        _this.$putAreaItem.style.backgroundImage = _this.$activeItem.style.backgroundImage;
+        _this.$activeItem.classList.add('accept');
+        _this.$activeItem.removeAttribute('draggable');
+        var count = 0;
+        for(var x = 0; x < _this.$container.childNodes.length; x++){
+            if(_this.$container.childNodes[x].classList.contains('accept') == true){
+                count++;
+            }
+        }
+        if(count == _this.$container.childNodes.length){
+            while (_this.$container.childNodes.length > 1) {
+                _this.$container.removeChild(_this.$container.lastChild);
+            }
+            while (_this.$container.childNodes.length > 1) {
+                _this.$container.removeChild(_this.$container.lastChild);
+            }
+            _this.$container.childNodes[0].classList.add('fullEnd');
+            // _this.$targetcontainer.childNodes[0].classList.add('fullEnd');
+            console.log(_this.$targetcontainer.childNodes.length);
+            for (var x=0; x < _this.$targetcontainer.childNodes.length; x++) {
+                console.log("x: "+x);
+                console.log(" _this.$targetcontainer.childNodes[x]: "+ _this.$targetcontainer.childNodes[x]);
+                console.log(" _this.$targetcontainer: "+ _this.$targetcontainer);
+                _this.$targetcontainer.childNodes[x].classList.add('clear');
+                _this.$targetcontainer.childNodes[x].innerHTML = '';
+            }
+            _timer.stop();
+            console.log(_timer);
+        }
+    }
+    else{
+        if (_this.$activeItem !== this) {
+        
+            _this.$activeItem.id = this.id;
+            _this.$activeItem.dataset.movePazleId = this.dataset.movePazleId;
+            _this.$activeItem.style.backgroundPosition = this.style.backgroundPosition;
+            var parseData = JSON.parse(event.dataTransfer.getData('text/plain'))
+            this.id = parseData.id;
+            this.dataset.movePazleId = parseData.dataMovePazzleId;
+            this.style.backgroundPosition = parseData.backgroundPosition;
+            
+          }
+    }
+    _this.removeClasses();
+  
+    return false;
+  };
+  
+  DragNSortProto.bind = function () {
+      this.on(this.$items, 'dragstart', this.onDragStart);
+      this.on(this.$items, 'dragend', this.onDragEnd);
+      this.on(this.$items, 'dragover', this.onDragOver);
+      this.on(this.$items, 'dragenter', this.onDragEnter);
+      this.on(this.$items, 'dragleave', this.onDragLeave);
+      this.on(this.$items, 'drop', this.onDrop);
+
+      this.on(this.$targetitems, 'dragover', this.onDragOver);
+      this.on(this.$targetitems, 'drop', this.onDrop);
+  };
+  
+  DragNSortProto.init = function () {
+      this.bind();
+  };
+  
+  // Instantiate
+  function Timer(delay, callbacks){
+    if (Object.prototype.toString.call(callbacks) === "[object Function]") {
+      callbacks = [callbacks];
+    }
+    this.callbacks = callbacks;
+    var that = this;
+    var id = setInterval(function tick(){
+      if (!that.running) return;
+      for (var i=0; i<that.callbacks.length; i++) {
+        that.callbacks[i].call(that);
+      }
+      that.count++;
+    }, delay);
+    Object.defineProperty(this, 'id', {
+        get: function() {
+          return id;
+        }
+      });
+    Object.defineProperty(this, 'delay', {
+        get: function() {
+            return delay;
+        }
+    });
+  }
+  Timer.prototype.running = false;
+  Timer.prototype.count = 0;
+  
+  Timer.prototype.run = function run(){
+    this.running = true;
+    return this;
+  };
+  Timer.prototype.stop = function stop(){
+    clearInterval(this.id);
+    this.stopped = true;
+    return this;
+  };
+  
+  
 
 startGameInstanceProto = null;
 document.addEventListener('DOMContentLoaded', function(){ 
